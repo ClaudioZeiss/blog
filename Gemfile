af117d6acf8d2dd6c86b2f5e5c18521927656f6f
Claudio Zeiss <claudio.zeiss@gmail.com>
2022-04-22 15:46:23 -0400

    2022.04.22_03

--- Gemfile
+++ Gemfile
@@ -12,6 +12,9 @@ gem "sprockets-rails"
 # Use postgresql as the database for Active Record
 gem "pg"
 
+#Boostrap
+gem "boostrap"
+
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", "~> 5.0"
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,10 @@
 class PostsController < ApplicationController
+
   def create
+    @post=Post.create(title: params[:titulo], image_url: params[:url_img], content:params[:contenido])
   end
 
   def index
+    @posts=Post.all.order("created_at DESC")
   end
 end
